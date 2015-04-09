6bd7717e704064e6c3e7b43ccf74921dc3afab1b
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-09 15:43:42 -0400

    answered question 6

---
+++ Question6/Question6/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Question6
+//
+//  Created by Daniel Kwiatkowski on 2015-04-09.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"You only have to write one methoh and it can apply to multiple vehicles you create");
+    }
+    return 0;
+}
