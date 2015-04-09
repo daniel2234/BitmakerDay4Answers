e61933520ab71c4dc8653406044cc22f5621cba3
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-09 14:06:01 -0400

    added the 5 actions for question 2

---
+++ Question2/Question2/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  Question2
+//
+//  Created by Daniel Kwiatkowski on 2015-04-09.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"The answer to question 2 is!");
+        
+        NSLog(@"Class; IPhone");
+        
+        NSLog(@"Action 1: call");
+        NSLog(@"Action 2: text");
+        NSLog(@"Action 3: game");
+        NSLog(@"Action 4: surf");
+        NSLog(@"Action 5: music");
+    }
+    return 0;
+}
