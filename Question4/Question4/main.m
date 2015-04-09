d8d8809475b9178412d2542ed00ebce333d64e28
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-09 15:18:52 -0400

    answered question 4

---
+++ Question4/Question4/main.m
@@ -0,0 +1,40 @@
+//
+//  main.m
+//  Question4
+//
+//  Created by Daniel Kwiatkowski on 2015-04-09.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Yes, there will be an overlap because a boat and a motorcycle will have accelerate, decelerate, overlap, refuel and wash!");
+        
+//        car
+//        - accelerate
+//        - decelerate
+//        - refuel
+//        - wash
+//        
+//        boat
+//        - accelerate
+//        - decelerate
+//        - refuel
+//        - wash
+//        - deploySail
+//        - collapseSail
+//        
+//        motorcycle
+//        - accelerate
+//        - decelerate
+//        - refuel
+//        - wash
+//        - popWheelie
+
+        
+    }
+    return 0;
+}
