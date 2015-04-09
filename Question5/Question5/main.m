58132ee873c63b70b4703a80a8297e37be028bdb
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-09 15:40:32 -0400

    answered question 5

---
+++ Question5/Question5/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Question5
+//
+//  Created by Daniel Kwiatkowski on 2015-04-09.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"You can use the same set of methods for all three objects");
+    }
+    return 0;
+}
